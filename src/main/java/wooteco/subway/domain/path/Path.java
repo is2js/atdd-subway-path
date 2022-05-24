8f04aa90d92e8081bee0546917a396ea794892a6
is2js <tingstyle1@gmail.com>
2022-05-24 16:52:15 +0900

    feat(domain/Path): 거리에 따른 요금 계산 기능 및 test 구현

--- src/main/java/wooteco/subway/domain/path/Path.java
+++ src/main/java/wooteco/subway/domain/path/Path.java
@@ -5,12 +5,35 @@ import java.util.List;
 public class Path {
 
     private final List<Long> stationIds;
-    private final double distance;
+    private final int distance;
 
-
-    public Path(final List<Long> stationIds, final double distance) {
+    public Path(final List<Long> stationIds, final int distance) {
         this.stationIds = stationIds;
         this.distance = distance;
+    }
+
+    public int calculateFare() {
+        final int defaultFare = 1250;
+        if (distance <= 10) {
+            return defaultFare;
+        }
+        if (10 < distance) {
+            return defaultFare + calculateOverFare(10, 5);
+        }
+        return defaultFare + calculateOverFare(10, 5) + calculateOverFare(50, 8);
+    }
+
+    private int calculateOverFare(final int standardDistance, final int unitDistance) {
+        final int overDistance = distance - standardDistance;
+        return (int) (Math.ceil((overDistance - 1) / unitDistance) + 1) * 100;
+    }
+
+    public List<Long> getStationIds() {
+        return stationIds;
+    }
+
+    public double getDistance() {
+        return distance;
     }
 
     @Override
---
+++ src/test/java/wooteco/subway/domain/path/PathTest.java
@@ -0,0 +1,24 @@
+package wooteco.subway.domain.path;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class PathTest {
+
+    @DisplayName("")
+    @ParameterizedTest
+    @CsvSource(value = {"9,1250", "10,1250", "11,1350", "15,1350", "16, 1450", "50, 2050", "51,2150"})
+    void calculateFare(final int distance, final int expected) {
+
+        final Path path = new Path(List.of(1L, 2L, 3L), distance);
+        final int actual = path.calculateFare();
+
+        Assertions.assertThat(actual).isEqualTo(expected);
+
+        System.out.println("actual = " + actual);
+        System.out.println("expected = " + expected);
+    }
+}
