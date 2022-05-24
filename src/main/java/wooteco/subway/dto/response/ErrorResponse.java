d83e6c21fbf48ca8345424038066e8156026d25f
is2js <tingstyle1@gmail.com>
2022-05-24 17:50:42 +0900

    feat: status이외 errormessage body에 응답하는 ErrorReponse(DTO) 구현

---
+++ src/main/java/wooteco/subway/dto/response/ErrorResponse.java
@@ -0,0 +1,13 @@
+package wooteco.subway.dto.response;
+
+public class ErrorResponse {
+
+    private String message;
+
+    public ErrorResponse() {
+    }
+
+    public ErrorResponse(final String message) {
+        this.message = message;
+    }
+}
--- src/main/java/wooteco/subway/ui/ControllerAdvice.java
+++ src/main/java/wooteco/subway/ui/ControllerAdvice.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import wooteco.subway.dto.response.ErrorResponse;
 import wooteco.subway.exception.LineDuplicateException;
 import wooteco.subway.exception.LineNotFoundException;
 import wooteco.subway.exception.SectionNotFoundException;
@@ -15,24 +16,27 @@ import wooteco.subway.exception.StationNotFoundException;
 public class ControllerAdvice {
 
     @ExceptionHandler(value = IllegalArgumentException.class)
-    public ResponseEntity handleIllegalException(Exception e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    public ResponseEntity<ErrorResponse> handleIllegalException(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(value = {LineNotFoundException.class, StationNotFoundException.class,
         SectionNotFoundException.class})
-    public ResponseEntity handleNotFoundException(Exception e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    public ResponseEntity<ErrorResponse> handleNotFoundException(Exception e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(value = {LineDuplicateException.class, StationDuplicateException.class})
-    public ResponseEntity handleDuplicateException(Exception e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    public ResponseEntity<ErrorResponse> handleDuplicateException(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(value = Exception.class)
-    public ResponseEntity handleUnexpectedException(Exception e) {
+    public ResponseEntity<ErrorResponse> handleUnexpectedException(Exception e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .build();
+            .body(new ErrorResponse(e.getMessage()));
     }
 }
