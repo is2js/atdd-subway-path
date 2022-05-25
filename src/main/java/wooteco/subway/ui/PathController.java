58bea4b97719667c1b06e0c4257708e061be40b9
is2js <tingstyle1@gmail.com>
2022-05-25 18:02:28 +0900

    feat(ui/PathController): path controller 기능 구현

---
+++ src/main/java/wooteco/subway/ui/PathController.java
@@ -0,0 +1,37 @@
+package wooteco.subway.ui;
+
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wooteco.subway.domain.Station;
+import wooteco.subway.domain.path.Path;
+import wooteco.subway.dto.request.PathRequest;
+import wooteco.subway.dto.response.PathResponse;
+import wooteco.subway.service.PathService;
+import wooteco.subway.service.StationService;
+
+@RestController
+@RequestMapping("/paths")
+public class PathController {
+
+    private final PathService pathService;
+    private final StationService stationService;
+
+    public PathController(final PathService pathService, final StationService stationService) {
+        this.pathService = pathService;
+        this.stationService = stationService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<PathResponse> showPath(PathRequest pathRequest) {
+        final Path path = pathService.show(pathRequest);
+
+        final List<Station> stations = stationService.findByIds(path.getStationIds());
+
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(PathResponse.from(stations, path));
+    }
+}
